876ed64d2ddbff2e3dcd503c67df9cddbe344029
Pawnman <maks-peshkov@mail.ru>
2024-05-22 12:00:21 +0300

    UI
    Added a couple scenes with simple UI: Start, Story, Controll, Finish.
    So, Game has a full cycle: Start - Play - Finish - Restart.

---
+++ Assets/DarkLab/Scripts/MusicManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    private static MusicManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Level_1")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
